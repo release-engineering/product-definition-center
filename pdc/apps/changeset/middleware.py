7eb64878543ec18f3a7cb2d0a1e6041be0e023b3
lao605 <zlao@redhat.com>
2015-07-31 17:18:20 +0800

    Return response directly rather than using an else statement

--- pdc/apps/changeset/middleware.py
+++ pdc/apps/changeset/middleware.py
@@ -42,30 +42,31 @@ class ChangesetMiddleware(object):
                                            ensure_ascii=False)
                 response = HttpResponse(response_data, content_type='application/json')
                 response.status_code = status.HTTP_405_METHOD_NOT_ALLOWED
-            else:
-                logger.debug("Start write request on the view %s." % view_func.__name__)
-                try:
-                    with transaction.atomic():
-                        request.changeset = models.Changeset(author=user)
-                        response = view_func(request, *view_args, **view_kwargs)
-                        # response.exception=True means there is an error occurs.
-                        if getattr(response, 'exception', 0) or (
-                            hasattr(response, 'status_code') and response.status_code >= 400
-                        ):
-                            # avoid recording changeset on server error, also
-                            # abort the transaction so that no modifications are
-                            # done to database
-                            request.changeset.reset()
-                            transaction.set_rollback(True)
-                        else:
-                            request.changeset.commit()
-                except:
-                    # NOTE: catch all errors that were raised by view.
-                    # And log the trace back to the file.
-                    logger.error('View Function Error: %s', request.path,
-                                 exc_info=sys.exc_info())
-                    # we do not want to break the default exception processing chains,
-                    # so re-raise the exception to the upper level.
-                    raise
+                return response
+
+            logger.debug("Start write request on the view %s." % view_func.__name__)
+            try:
+                with transaction.atomic():
+                    request.changeset = models.Changeset(author=user)
+                    response = view_func(request, *view_args, **view_kwargs)
+                    # response.exception=True means there is an error occurs.
+                    if getattr(response, 'exception', 0) or (
+                        hasattr(response, 'status_code') and response.status_code >= 400
+                    ):
+                        # avoid recording changeset on server error, also
+                        # abort the transaction so that no modifications are
+                        # done to database
+                        request.changeset.reset()
+                        transaction.set_rollback(True)
+                    else:
+                        request.changeset.commit()
+            except:
+                # NOTE: catch all errors that were raised by view.
+                # And log the trace back to the file.
+                logger.error('View Function Error: %s', request.path,
+                             exc_info=sys.exc_info())
+                # we do not want to break the default exception processing chains,
+                # so re-raise the exception to the upper level.
+                raise
 
         return response
