c13af1c1386efe5797bd5dfafdd3e691212d2b43
Ralph Bean <rbean@redhat.com>
2016-03-16 20:50:11 -0400

    Avoid double-encoding the fedmsg json payload.

--- pdc/apps/utils/messaging.py
+++ pdc/apps/utils/messaging.py
@@ -4,6 +4,7 @@
 # http://opensource.org/licenses/MIT
 #
 from django.conf import settings
+from django.utils import six
 
 import logging
 
@@ -36,7 +37,10 @@ class FedmsgMessenger(object):
         self.messenger = fedmsg
 
     def send_message(self, topic, msg):
-        self.messenger.publish(topic=topic.strip('.'), msg=msg)
+        topic = topic.strip('.')
+        if isinstance(msg, six.string_types):
+            msg = json.loads(msg)
+        self.messenger.publish(topic=topic, msg=msg)
 
 
 class ProtonMessenger(object):
